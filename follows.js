b71b8c445f65d21ee9e268a851710d16420040f6
Railaga <ralara@uniboyaca.edu.co>
2015-04-13 09:09:37 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'X82FnL3ya8Jcj29VnjYcZRzGFq6dPWXDDUr9s4Z6lUDIxuu6', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [24125668], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
